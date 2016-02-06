0114dffe05c65e995713832aea07e99b84231ace
Ryan Wright <ryanwrightdev@gmail.com>
2016-02-06 02:18:11 -0500

    make sure apt-get is the first thing to run

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,4 +1,8 @@
-
+exec{ "apt-update":
+  command => '/usr/bin/apt-get update'
+}
+#make sure to run apt-get update before installing any packages.
+Exec['apt-update'] -> Package <| |>
 
 class{'zendframework':
 }
